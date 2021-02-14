53cf6d534443fb0abb7bdba17d64f1216661f92e
Gyeong-ah Na <60082435+GyeongahNa@users.noreply.github.com>
2021-02-15 00:15:30 +0900

    get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -15,13 +15,13 @@
 
 # include <stddef.h>
 
-char	*ft_strcpy(char *dest, const char *src);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, const char *s2);
-char	*ft_strchr(const char *str, int c);
-size_t	ft_strlen(const char *str);
-char	*ft_strcat(char *dest, const char *src);
-int		get_next_line(int fd, char **line);
+char	  *ft_strcpy(char *dest, const char *src);
+char	  *ft_strdup(const char *s);
+char	  *ft_substr(char const *s, unsigned int start, size_t len);
+char	  *ft_strjoin(char const *s1, const char *s2);
+char	  *ft_strchr(const char *str, int c);
+size_t  ft_strlen(const char *str);
+char    *ft_strcat(char *dest, const char *src);
+int	    get_next_line(int fd, char **line);
 
 #endif
